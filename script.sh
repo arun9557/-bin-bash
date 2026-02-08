69b5b46c8d1d17de024d84a5a67df38f00f74c02
A₹UN <117110654+arun9557@users.noreply.github.com>
2026-02-08 16:30:29 +0530

    my_srt

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+myname="Arun"
+myclass="nd"
+date=$(date)
+net=$(ping -c 4 google.com)
+
+echo "Hey, my name is $myname."
+echo "If you wanna know about me ------ let's look at this post."
+
+echo "These days, I am learning about CyberSecurity | Pentesting | Ethical Hacking | Red Team | Blue Team."
+echo "This post was created on $date."
+echo "My class is $myclass. I don’t want to join any formal class — I am learning on my own."
+echo "I am not passionate about chasing a degree or going to college like kids of STUK."
+
+echo ""
+
+echo "IF u guys are interested in learning about CyberSecurity | Pentesting |Web hacking "
+echo "so , learn all the skill and start from basic | learn all the tools | and start your journey "
+echo "Like ---"
+echo "here is some linux cmd , ping | curl | nmap | sqlmap | dirb | hydra | sqlmap | "
+echo ""
+
+echo "$net , here usse ping cmd to check the network connection on google.com"
+
