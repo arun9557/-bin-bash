7fb8ca8171b8cda2a35d518fe276c6cff53e56d8
A₹UN <117110654+arun9557@users.noreply.github.com>
2026-02-09 19:27:45 +0530

    br hy , expoet

---
+++ brutforce_attac.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Brute Force Attack: login page - "
+
+
+attak="$hydra <domain.name> -L <usernames.txt> -P <passwords.txt> -f -o <output.txt>"
+
+echo "hydra is running now baby $attak"
+
+echo "$brupsuite ... opning bro.. "
+echo "set proxy - >"
+
+user="username"
+pass="password"
+
+
+
+
