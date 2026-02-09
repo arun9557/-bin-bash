55f853a54c2d904edc26eab29fae2ed752b6a1ff
A₹UN <117110654+arun9557@users.noreply.github.com>
2026-02-09 11:13:08 +0530

    value checker

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+value=$(cat /proc/cpuinfo | grep "model name" | head -1 | awk -F: '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
+echo "CPU Model: $value"
+
+
+
+
+chechind_numvers=300
+if [ $chechind_numvers -gt 100 ]; then
+    echo "The number is greater than 100."
+else
+    echo "The number is less than or equal to 100."
+fi
