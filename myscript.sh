c641c35de561990384d3e0a3972fcece4eb8c954
A₹UN <117110654+arun9557@users.noreply.github.com>
2026-02-09 12:42:25 +0530

    value checker

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash   .5
 
 value=$(cat /proc/cpuinfo | grep "model name" | head -1 | awk -F: '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//')
 echo "CPU Model: $value"
@@ -11,4 +11,10 @@ if [ $chechind_numvers -gt 100 ]; then
     echo "The number is greater than 100."
 else
     echo "The number is less than or equal to 100."
+fi
+
+if [ -f ~/my_file.txt ]; then
+    echo "The file exists."
+else
+    echo "The file does not exist."
 fi
